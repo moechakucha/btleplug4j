6eaa73a2fabde8e6fbe2d1a67b5f0463b23a1ea9
Flamarine <Flamarine@outlook.com>
2026-06-25 20:03:35 +0800

    build: revert add aarch64-unknown-linux-gnu target

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,6 @@ val targetPlatforms = listOf(
 	PlatformInfo("macos", "x86_64", "x86_64-apple-darwin", "dylib", "lib"),
 	PlatformInfo("windows", "x86_64", "x86_64-pc-windows-gnu", "dll", ""),
 	PlatformInfo("linux", "x86_64", "x86_64-unknown-linux-gnu", "so", "lib"),
-	PlatformInfo("linux", "aarch64", "aarch64-unknown-linux-gnu", "so", "lib"),
 )
 
 data class PlatformInfo(
